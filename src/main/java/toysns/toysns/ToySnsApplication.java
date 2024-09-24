369249f55eaabf19bc4bd35236617ea6e7449291
GoGradually <chkun3109@gmail.com>
2024-09-25 08:56:32 +0900

    ToySNS 시작

---
+++ src/main/java/toysns/toysns/ToySnsApplication.java
@@ -0,0 +1,13 @@
+package toysns.toysns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToySnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToySnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toysns/toysns/ToySnsApplicationTests.java
@@ -0,0 +1,13 @@
+package toysns.toysns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToySnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
