f74ee256e97ab8e579d74791b7a2e58eeab0fe7b
GoGradually <chkun3109@gmail.com>
2024-10-06 15:08:21 +0900

    MemberQueryRepositoryTest.java 작성

--- src/test/java/toysns/toysns/domain/member/repository/MemberQueryRepositoryTest.java
+++ src/test/java/toysns/toysns/domain/member/repository/MemberQueryRepositoryTest.java
@@ -1,6 +1,8 @@
 package toysns.toysns.domain.member.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -8,6 +10,9 @@ import org.springframework.transaction.annotation.Transactional;
 import toysns.toysns.domain.member.Address;
 import toysns.toysns.domain.member.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -20,6 +25,14 @@ class MemberQueryRepositoryTest {
 
     @BeforeEach
     void setUp(){
+        for (int i = 0; i <2; i++) {
+            Member member = Member.builder()
+                    .username("tesst" + i)
+                    .email("tesst" + i + "@email.com")
+                    .introduce("hello")
+                    .address(new Address("", "", ""))
+                    .build();
+        }
         for (int i = 0; i < 10; i++) {
             Member member = Member.builder()
                     .username("test" + i)
@@ -29,5 +42,99 @@ class MemberQueryRepositoryTest {
                     .build();
             em.persist(member);
         }
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    void findMembersByUsername_10개_조회(){
+        //given
+
+        //when
+        List<Member> findMembers = memberQueryRepository.findMembersByUsername("test", null);
+        //then
+
+        assertThat(findMembers).hasSize(10);
+        assertThat(findMembers).allMatch(m -> m.getUsername().startsWith("test"));
+    }
+    @Test
+    void findMembersByUsername_0개_조회(){
+        //given
+
+        //when
+        List<Member> findMembers = memberQueryRepository.findMembersByUsername("nothing", null);
+
+        //then
+        assertThat(findMembers).isNotNull();
+        assertThat(findMembers).hasSize(0);
+    }
+
+    @Test
+    void findMembersByUsername_10개_조회_limit(){
+        //given
+        for (int i = 10; i < 20; i++) {
+            Member member = Member.builder()
+                    .username("test" + i)
+                    .email("test" + i + "@email.com")
+                    .introduce("hello")
+                    .address(new Address("", "", ""))
+                    .build();
+            em.persist(member);
+        }
+        em.flush();
+        em.clear();
+        //when
+        List<Member> findMembers = memberQueryRepository.findMembersByUsername("test", null);
+
+        //then
+        assertThat(findMembers).hasSize(10);
+        assertThat(findMembers).allMatch(m -> m.getUsername().startsWith("test"));
+
+    }
+    @Test
+    void findMembersByUsername_1개_조회(){
+        //when
+        List<Member> findMembers = memberQueryRepository.findMembersByUsername("test5", null);
+
+        //then
+        assertThat(findMembers).hasSize(1);
+        assertThat(findMembers).allMatch(m -> m.getUsername().startsWith("test5"));
+    }
+
+    @Test
+    void findMembersByUsername_5개_조회_lastUsername(){
+        //given
+
+        //when
+        List<Member> findMembers = memberQueryRepository.findMembersByUsername("test", "test4");
+
+        //then
+        assertThat(findMembers).hasSize(5);
+        assertThat(findMembers).allMatch(m -> m.getUsername().startsWith("test"));
+        assertThat(findMembers).allMatch(m->m.getUsername().compareTo("test4") > 0);
+    }
+
+    @Test
+    void findMembersByUsername_10개_조회_limit_lastUsername(){
+        //given
+        for (int i = 10; i < 20; i++) {
+            Member member = Member.builder()
+                    .username("test" + i)
+                    .email("test" + i + "@email.com")
+                    .introduce("hello")
+                    .address(new Address("", "", ""))
+                    .build();
+            em.persist(member);
+        }
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> findMembers = memberQueryRepository.findMembersByUsername("test", "test18");
+
+        //then
+        assertThat(findMembers).hasSize(10);
+        assertThat(findMembers).allMatch(m -> m.getUsername().startsWith("test"));
+        assertThat(findMembers).allMatch(m->m.getUsername().compareTo("test18") > 0);
     }
 }
