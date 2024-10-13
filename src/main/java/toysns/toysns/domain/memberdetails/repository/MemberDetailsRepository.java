4c8bae8a645afaf7a806c9b34d9d084322cc536e
GoGradually <chkun3109@gmail.com>
2024-10-13 19:05:53 +0900

    MemberDetails 제거 -> 추후 제대로 작성

--- src/main/java/toysns/toysns/domain/memberdetails/repository/MemberDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package toysns.toysns.domain.memberdetails.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import toysns.toysns.domain.memberdetails.MemberDetails;
-
-@Repository
-public interface MemberDetailsRepository extends JpaRepository<MemberDetails, Long> {
-}
